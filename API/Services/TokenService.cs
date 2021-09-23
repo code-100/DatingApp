e0358d722af5baa82144bf40dcaf40d0344cf039
Amarpreet Singh <singh.amarpreet.1999@gmail.com>
2021-09-24 05:09:52 +0530

    remove error from token service

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -32,7 +32,7 @@ namespace API.Services
                 Subject = new ClaimsIdentity(claims),
                 Expires = DateTime.Now.AddDays(7),
                 SigningCredentials = creds
-            }
+            };
 
             var tokenHandler = new JwtSecurityTokenHandler();
             
